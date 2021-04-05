9c61fd8919c2e9e612ba3f9a868ea09d59305b6b
Horsfalc <Horsfalc@uni.coventry.ac.uk>
2021-04-05 02:19:23 +0100

    mouselistener added for person table

--- Horsfalc-CW/src/Clientpkg/Client.java
+++ Horsfalc-CW/src/Clientpkg/Client.java
@@ -389,15 +389,15 @@ public class Client {
              public void mouseClicked(MouseEvent e) {
 
                  personTable = (JTable)e.getSource();
-                 int row = personTable.rowAtPoint( e.getPoint() );
+                 int rows = personTable.rowAtPoint( e.getPoint() );
 
-                 textFieldPersonId.setText(String.valueOf(personTable.getModel().getValueAt(row, 0)));
+                 textFieldPersonId.setText(String.valueOf(personTable.getModel().getValueAt(rows, 0)));
 
-                 textFieldFirstName.setText(String.valueOf(personTable.getModel().getValueAt(row, 1)));
+                 textFieldFirstName.setText(String.valueOf(personTable.getModel().getValueAt(rows, 1)));
 
-                 textFieldLastName.setText(String.valueOf(personTable.getModel().getValueAt(row, 2)));
+                 textFieldLastName.setText(String.valueOf(personTable.getModel().getValueAt(rows, 2)));
 
-                 textFieldLibraryCard.setText(String.valueOf(personTable.getModel().getValueAt(row, 3)));
+                 textFieldLibraryCard.setText(String.valueOf(personTable.getModel().getValueAt(rows, 3)));
 
              }
 
