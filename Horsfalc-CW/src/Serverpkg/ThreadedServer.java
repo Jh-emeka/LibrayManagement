3e46b5dea52894aa45a4092609a9e2c4a12bcdd0
Horsfalc <Horsfalc@uni.coventry.ac.uk>
2021-04-05 02:41:42 +0100

    Added three synchronised functions to select from the three tables in DB

--- Horsfalc-CW/src/Serverpkg/ThreadedServer.java
+++ Horsfalc-CW/src/Serverpkg/ThreadedServer.java
@@ -1,5 +1,7 @@
 package Serverpkg;
 import both.Book;
+import both.On_loan;
+import both.Person;
 
 
 import java.io.IOException;
@@ -9,6 +11,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -49,7 +53,8 @@ public class ThreadedServer {
             ResultSet resultSet = prep.executeQuery();
 
             while (resultSet.next()) {
-                books.add(Book.newTrackFromResultSet(resultSet));
+
+                books.add(Book.newBookFromResultSet(resultSet));
             }
         } catch (SQLException ex) {
             Logger.getLogger(Serverpkg.ThreadedServer.class.getName()).log(Level.SEVERE, null, ex);
@@ -57,6 +62,46 @@ public class ThreadedServer {
         return books;
     }
 
+    public synchronized List<Person> initPerson() {
+
+        ArrayList<Person> person = new ArrayList<>();
+        String selectSQL = "SELECT * FROM person "; // lets just get the first 10 records for testing
+
+
+        try (Connection conn = ConnectionFactory.getConnection(); // auto close the connection object after try
+             PreparedStatement prep = conn.prepareStatement(selectSQL)) {
+
+            ResultSet resultSet = prep.executeQuery();
+
+            while (resultSet.next()) {
+                person.add(Person.newPersonFromResultSet(resultSet));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Serverpkg.ThreadedServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return person;
+    }
+
+    public synchronized List<On_loan> initOnLoan() {
+
+        ArrayList<On_loan> onLoan = new ArrayList<>();
+        String selectSQL = "SELECT * FROM on_loan "; // lets just get the first 10 records for testing
+
+
+        try (Connection conn = ConnectionFactory.getConnection(); // auto close the connection object after try
+             PreparedStatement prep = conn.prepareStatement(selectSQL)) {
+
+            ResultSet resultSet = prep.executeQuery();
+
+            while (resultSet.next()) {
+
+                onLoan.add(On_loan.newOnLoanFromResultSet(resultSet));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Serverpkg.ThreadedServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return onLoan;
+    }
 
 
     /**
@@ -66,7 +111,7 @@ public class ThreadedServer {
     private void connectToClients() {
         System.out.println("Server: Server starting.");
 
-        try (ServerSocket serverSocket = new ServerSocket(2200)) {
+        try (ServerSocket serverSocket = new ServerSocket(3000)) {
 
             while (true) {
                 System.out.println("Server: Waiting for connecting client...");
@@ -74,7 +119,7 @@ public class ThreadedServer {
                 try {
                     Socket socket = serverSocket.accept();
 
-                    ClientHandler clientHandler = new ClientHandler(socket, initBooks() );
+                    ClientHandler clientHandler = new ClientHandler(socket,initBooks(),initPerson(),initOnLoan());
                     Thread connectionThread = new Thread(clientHandler);
                     connectionThread.start(); // thread goes into a ready state
                 } catch (IOException ex) {
