6c17f102db4b6ec9fbbdefc8b59568ed89767bb2
Horsfalc <Horsfalc@uni.coventry.ac.uk>
2021-04-23 12:46:32 +0100

    function for handling delete added to client handler

--- Horsfalc-CW/src/Serverpkg/ClientHandler.java
+++ Horsfalc-CW/src/Serverpkg/ClientHandler.java
@@ -166,6 +166,18 @@ public class ClientHandler implements Runnable {
 
                 }
 
+                if((parcelRead.getCommand() == Command.DELETE) && (parcelRead.getTable() == Table.PERSON)){
+
+
+                    ThreadedServer.deletePerson((Person) parcelRead.getNewData());
+
+                    ack = new Parcel(Command.SUCCESS);
+
+                    objectOutputStream.writeObject(ack);
+
+
+                }
+
             }
         } catch (SocketException | EOFException ex) {
             System.out.println("Server: We have lost connection to client " + connectionCount + ".");
